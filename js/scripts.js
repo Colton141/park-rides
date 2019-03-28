b3a046d9dfdb44139d4ecde30a1b277f57572ac9
Alex Martinez and Colton Lacey <alex@>
2019-03-28 13:46:43 -0700

    fix if else in js

--- js/scripts.js
+++ js/scripts.js
@@ -2,26 +2,33 @@ $(document).ready(function(){
   $("button#submit").click(function(event){
     var height = parseInt($("#height").val());
 
-    if (height >= 3){
+    if (height >= 3 && height < 4){
       $(".s").show();
       $(".m").hide();
       $(".l").hide();
       $(".x").hide();
-    }
-   if (height >= 4){
+      $(".too-short").hide();
+    } else if (height >= 4 && height < 5){
       $(".m").show();
       $(".s").hide();
       $(".l").hide();
       $(".x").hide();
-    }
-   if (height >= 5){
+      $(".too-short").hide();
+    } else if (height >= 5 && height < 6){
       $(".l").show();
       $(".s").hide();
       $(".m").hide();
       $(".x").hide();
-    }
-   if (height >= 6){
+      $(".too-short").hide();
+    } else if (height >= 6){
       $(".x").show();
+      $(".l").hide();
+      $(".s").hide();
+      $(".m").hide();
+      $(".too-short").hide();
+    } else {
+      $(".too-short").show();
+      $(".x").hide();
       $(".l").hide();
       $(".s").hide();
       $(".m").hide();
